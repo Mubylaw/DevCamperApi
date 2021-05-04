a06c60ca550a2b4403794c7cb389f8afae23ac1d
Mubarak Lawal <mubylaww@gmail.com>
2021-05-04 22:33:09 +0100

    created routes, controllers and middleware

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require('morgan')
+
+// route files
+const bootcamp = require('./routes/bootcamps');
 
 // load env vars
 dotenv.config({path: './config/config.env' });
 
 const app = express();
+// dev logging middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+app.use('/api/v1/bootcamps', bootcamp);
 
 const PORT = process.env.PORT || 5000;
 
